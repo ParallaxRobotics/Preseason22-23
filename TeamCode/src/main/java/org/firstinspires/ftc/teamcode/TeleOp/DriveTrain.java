abe7a5b7cd815fb4aa1422dca3f8c152af68dd96
PrathikI <iyengar.prathik@gmail.com>
2022-09-17 20:41:35 -0400

    Classes created
    HardwareRobot and DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveTrain.java
@@ -1,69 +1,60 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.HardwareRobot;
 
-@TeleOp
-public class DriveTrain extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
+import java.sql.Driver;
 
-        // Declare our motors
-        DcMotor FL = hardwareMap.dcMotor.get("FL");   // Port:
-        DcMotor BL = hardwareMap.dcMotor.get("BL");   // Port:
-        DcMotor FR = hardwareMap.dcMotor.get("FR");   // Port:
-        DcMotor BR = hardwareMap.dcMotor.get("BR");   // Port:
-        Servo tailServo = hardwareMap.servo.get("tailServo");   // Port:
+public class DriveTrain {
 
-        // Reverse the right side motors
-        FR.setDirection(DcMotorSimple.Direction.REVERSE);
-        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+    HardwareRobot robot;
+    public ElapsedTime timer;
+    double DriveSen = 0.6;
 
-        waitForStart();
+    public  enum DriveState {
+        START,
+        IDLE,
+        FORWARDS,
+        STOP,
+        BACKWARDS
+    }
 
-        if (isStopRequested()) return;
+    public static DriveState state = DriveState.IDLE;
 
-        while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y;
-            double x = -gamepad1.left_stick_x;
-            double rx = -0.9 * gamepad1.right_stick_x;
+    public DriveTrain(HardwareRobot r, ElapsedTime timer) {
+        robot = r;
+        this.timer = timer;
+    }
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-            double frontLeftPower = 0;
-            double backLeftPower = 0;
-            double frontRightPower = 0;
-            double backRightPower = 0;
-            double DriveSen = 1.0;
+    public void Update(Gamepad gamepad1, Gamepad gamepad2) {
 
-            if (denominator == 0) {
-                FL.setPower(0);
-                FR.setPower(0);
-                BL.setPower(0);
-                BR.setPower(0);
+        double y = gamepad1.left_stick_x;
+        double x = -gamepad1.left_stick_y;
+        double rx = 0.7 * gamepad1.right_stick_x;
 
-            } else {
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
-                frontLeftPower = (y + x + rx) / denominator;
-                backLeftPower = (y - x + rx) / denominator;
-                frontRightPower = (y - x - rx) / denominator;
-                backRightPower = -(y + x - rx) / denominator;
-
-                FL.setPower(DriveSen * frontLeftPower);
-                FR.setPower(DriveSen * frontRightPower);
-                BL.setPower(DriveSen * backLeftPower);
-                BR.setPower(DriveSen * backRightPower);
-            }
-
-            if (gamepad1.a) {
-                tailServo.setPosition(0.5);
-
-            } else {
-                tailServo.setPosition(0.2);
-            }
+        if (denominator == 0) {
+            robot.frontLeft.setPower(0);
+            robot.frontRight.setPower(0);
+            robot.backLeft.setPower(0);
+            robot.backRight.setPower(0);
         }
+
+        else {
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x - rx) / denominator;
+            double frontRightPower = (y - x + rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            robot.frontLeft.setPower(DriveSen * frontLeftPower);
+            robot.frontRight.setPower(DriveSen * frontRightPower);
+            robot.backLeft.setPower(DriveSen * backLeftPower);
+            robot.backRight.setPower(DriveSen * backRightPower);
+        }
+
+
     }
 }
